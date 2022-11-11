4567a83ef3c3a308d191d99b7de93727357fde6f
gowtamkumar <gowtampaul0@gmail.com>
2022-11-11 09:03:03 +0600

    orders

--- components/Navigations/DrawerNavigation/index.jsx
+++ components/Navigations/DrawerNavigation/index.jsx
@@ -3,6 +3,8 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import Login from "../../../screens/login";
 import Signup from "../../../screens/signup";
 import TabNavigation from "../TabNavigation";
+import Contact from "../../../screens/Contact";
+import Orders from "../../../screens/Orders";
 
 export default function DrawerNavigation() {
   const Drawer = createDrawerNavigator();
@@ -11,6 +13,8 @@ export default function DrawerNavigation() {
       <Drawer.Screen name="HomeTab" component={TabNavigation} />
       <Drawer.Screen name="Login" component={Login} />
       <Drawer.Screen name="Signup" component={Signup} />
+      <Drawer.Screen name="Contact" component={Contact} />
+      <Drawer.Screen name="Orders" component={Orders} />
     </Drawer.Navigator>
   );
 }
